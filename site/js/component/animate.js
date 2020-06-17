5c6566b8510f0c04d4f8de64de9a2f0ec6ac3f83
FroggyWeb <d.trezkov@gmail.com>
2020-06-17 13:41:00 +0300

    fix mobile

--- site/js/component/animate.js
+++ site/js/component/animate.js
@@ -1,60 +1,68 @@
-import ScrollMagic from 'scrollmagic';
-import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import ScrollMagic from "scrollmagic";
+import "imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap";
 // import {  } from './../vendor/TweenMax.min.js';
-import {TweenMax, TimelineMax } from 'gsap';
+import { TweenMax, TimelineMax } from "gsap";
 
 let controller = new ScrollMagic.Controller(),
-    book = '.intro__book',
-    phone = '.intro__phone',
-    pad = '.intro__pad',
-    introTween = new TimelineMax(),
-    skillsListTween = new TimelineMax()
+  book = ".intro__book",
+  phone = ".intro__phone",
+  pad = ".intro__pad",
+  introTween = new TimelineMax(),
+  skillsListTween = new TimelineMax();
 
 // Intro slider animate-------------------------------------
 introTween
   .staggerFromTo(
-    [phone, pad, book], 1,
-    { x: 400, opacity: 0, delay: .7, ease: Elastic.easeOut},
-    {x: 0,  opacity: 1}, 0.3
+    [phone, pad, book],
+    1,
+    { x: 400, opacity: 0, delay: 0.7, ease: Elastic.easeOut },
+    { x: 0, opacity: 1 },
+    0.3
   )
 
-  .fromTo('.intro__title', .2,
-    {y: -100, opacity: 0},
-    {y: 0, opacity: 1}
-  )
+  .fromTo(".intro__title", 0.2, { y: -100, opacity: 0 }, { y: 0, opacity: 1 })
 
-  .staggerFromTo(".feature", .7,
-    {opacity: 0, scale:1.3, y: 100},
-    {opacity: 1, scale:1, y: 0}, .3
-  )
+  .staggerFromTo(
+    ".feature",
+    0.7,
+    { opacity: 0, scale: 1.3, y: 100 },
+    { opacity: 1, scale: 1, y: 0 },
+    0.3
+  );
 
 // Skill List animate-------------------------------------
 
-skillsListTween
-  .staggerFromTo('.skills-list__item', .5,
-    {opacity:0, scale:0},
-    {opacity:1, scale:1, ease: Power2.easeIn}, .3
-  )
+skillsListTween.staggerFromTo(
+  ".skills-list__item",
+  0.5,
+  { opacity: 0, scale: 0.7 },
+  { opacity: 1, scale: 1, ease: Power2.easeIn },
+  0.3
+);
 
 let skillList = new ScrollMagic.Scene({
   triggerElement: ".skills-list",
-  triggerHook: .8,
-  reverse: false
-  })
+  triggerHook: 0.8,
+  reverse: false,
+})
   .setTween(skillsListTween)
   .addTo(controller);
 
 // Work List animate-------------------------------------
 
-$('.works__item').each(function(){
-  let worksTween = TweenMax
-    .fromTo(this, Math.random()*5, {opacity:0}, {opacity: 1}, Math.random()*10)
+$(".works__item").each(function () {
+  let worksTween = TweenMax.fromTo(
+    this,
+    Math.random() * 5,
+    { opacity: 0 },
+    { opacity: 1 },
+    Math.random() * 10
+  );
   let scene = new ScrollMagic.Scene({
     triggerElement: this,
-    triggerHook: .8,
-    reverse: false
+    triggerHook: 0.8,
+    reverse: false,
   })
-  .setTween(worksTween)
-  .addTo(controller);
-})
-
+    .setTween(worksTween)
+    .addTo(controller);
+});
